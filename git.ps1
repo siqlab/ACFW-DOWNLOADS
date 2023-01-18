f9ee629307328cd49705d97923c5ba40873e203f
Aayush Rajthala <Aayushrajthala@gmail.com>
2023-01-18 01:52:36 -0800

    Modified git commands to bypass SSL Check

--- git.ps1
+++ git.ps1
@@ -1,10 +1,10 @@
 $commitName = $args[0]
 
 try {
-    git pull
+    git -c http.sslVerify=false pull
     git add .
     git commit -m "Added Downloaded Files of Test: $commitName"
-    git push    
+    git -c http.sslVerify=false push
 }
 catch {
     Write-Output "--ERROR--Exception Caught in git.ps1"

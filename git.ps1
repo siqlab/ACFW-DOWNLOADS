4691e24b79c763778c38a6b060cc1dcbda8fc875
Aayush Rajthala <Aayushrajthala@gmail.com>
2023-01-18 01:28:30 -0800

    Added Downloaded Files of Test: Formatted Code

--- git.ps1
+++ git.ps1
@@ -2,9 +2,9 @@ $commitName = $args[0]
 
 try {
     git pull
-git add .
-git commit -m "Added Downloaded Files of Test: $commitName"
-git push    
+    git add .
+    git commit -m "Added Downloaded Files of Test: $commitName"
+    git push    
 }
 catch {
     Write-Output "--ERROR--Exception Caught in git.ps1"
